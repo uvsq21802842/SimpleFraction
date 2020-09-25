99c933ae8aa80fc1e1ff092c4b235328713cd35c
Eloïse MERCADO GARCIA <eloise.mercado-garcia@ens.uvsq.fr>
2020-09-25 10:03:07 +0200

    J'ai modifié le main et Fraction, avec toString()

--- src/Fraction.java
+++ src/Fraction.java
@@ -1,4 +1,33 @@
 
 public class Fraction {
+	private int numerateur;
+    private int denominateur;
 	
+    public Fraction(int n,int d){
+        this.setNumerateur(n);
+        this.setDenominateur(d);
+    }
+    
+    public int getDenominateur() {
+		return denominateur;
+	}
+	public void setDenominateur(int denominateur) {
+		if (denominateur == 0) {
+			System.out.print("Impossible");
+			 throw new ArithmeticException("denominator is zero");
+		}
+		this.denominateur = denominateur;
+	}
+	public int getNumerateur() {
+		return numerateur;
+	}
+	public void setNumerateur(int numerateur) {
+		this.numerateur = numerateur;
+	}
+	
+	@Override
+    public String toString(){
+        return getNumerateur()+"/"+getDenominateur();
+    }
+    
 }
--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,11 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println(" est connecté");
+		System.out.println("TRY : FRACTION");
+		Fraction f;
+		f = new Fraction(1,0);
+		System.out.println(f.toString()); 
+		
 	}
 
 }

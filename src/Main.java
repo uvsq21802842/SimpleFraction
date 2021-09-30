46733b72a0c2cc144c0a6c421fc5fad5261574cc
Utilisateur <user@debian.arcahe.ovh>
2021-09-30 15:24:41 +0200

    MàJ

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,6 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("TRY : FRACTION");
 		Fraction f;
 		f = new Fraction(1,1);
 		System.out.println(f.toString()); 
